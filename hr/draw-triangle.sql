d8b6a602c1174116ed96d0374c2c437bdefdc0af
Luis Rodriguez <lerdeveloper@gmail.com>
2024-10-28 19:18:55 +0000

    Adding "draw triangle" solution

---
+++ hr/draw-triangle.sql
@@ -0,0 +1,21 @@
+-- https://www.hackerrank.com/challenges/draw-the-triangle-1/problem?isFullScreen=true
+
+-- P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P
+-- (5):
+-- * * * * * 
+-- * * * * 
+-- * * * 
+-- * * 
+-- *
+
+with numbers as (
+    SELECT
+        20 AS n
+    UNION ALL
+        SELECT
+        n - 1
+    FROM numbers
+    where  n > 0
+)
+-- note the space between *
+select REPLICATE(' *', n)  from numbers;
